8f3ba9ad43c0a26a4231bd99ef3a83e97e0e29cd
Oleh Vasyura <vasyura.oleh@gmail.com>
2017-07-08 10:59:26 +0300

    Angular excersize

--- angular/conFusion/app/scripts/controllers.js
+++ angular/conFusion/app/scripts/controllers.js
@@ -66,13 +66,11 @@ angular.module('confusionApp')
             };
         }])
 
-        .controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+        .controller('DishDetailController', ['$scope', '$routeParams', 'menuFactory', function($scope, $routeParams, menuFactory) {
 
-            var orderBy = '';
-            var dish = menuFactory.getDish(3);
-            $scope.dish = dish;
-            
-        }])
+            var dish= menuFactory.getDish(parseInt($routeParams.id,10));                        $scope.dish = dish;
+                    }])
+    
 
         .controller('DishCommentController', ['$scope', function($scope) {
             
